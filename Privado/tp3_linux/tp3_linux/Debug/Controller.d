92cfd5b194e2b6b90918f808eee852182503f002
lorenaris1982 <52511676+lorenaris1982@users.noreply.github.com>
2019-11-10 20:03:36 -0300

    modifique

---
+++ Privado/tp3_linux/tp3_linux/Debug/Controller.d
@@ -0,0 +1,12 @@
+Controller.o: ../Controller.c ../LinkedList.h ../parser.h ../utn.h \
+ ../Controller.h ../Employee.h
+
+../LinkedList.h:
+
+../parser.h:
+
+../utn.h:
+
+../Controller.h:
+
+../Employee.h:
